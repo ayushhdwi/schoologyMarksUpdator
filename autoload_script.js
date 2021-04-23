cc853184ee5889682a8bdb010632767efedc9902
Ayush Dwivedi <parasdwivedi.dwivedi1@gmail.com>
2021-04-23 21:14:28 +0530

    added all of the code

---
+++ autoload_script.js
@@ -0,0 +1,8 @@
+// var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+console.log('Testing it');
+document.getElementsByTagName('video')[0].playbackRate = 2.5;
+
+function lol() {
+    document.getElementsByTagName("video")[0].playbackRate = 2.5;
+}
---
+++ popup.js
@@ -0,0 +1,3 @@
+function lol() {
+	document.getElementsByTagName('video')[0].playbackRate = 2.5;
+}
